c3f08ad6099133e560c926c0b92a2b336cad1975
aliabuhumra <alihhmmre.a@gmail.com>
2022-08-02 03:35:47 +0300

    first

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Navbar.css';
+
+// import BiMenuAltLeft from 'react-icons/md';
+
+function Navbar() {
+
+  // const [click, setClick] = useState(false);
+
+  return (
+    <div className='nav-bar'>
+      <div className="logo">
+          <img src="images/LOGO.svg" alt="" />
+      </div>
+      {/* <div className="icon-bar">
+        <span><BiMenuAltLeft/></span>
+      </div> */}
+      <div className="list">
+        <ul>
+           
+            <li><a>الرئيسيه</a></li>
+            <li>من نحن</li>
+            <li>ماذا نقدم</li>
+            <li>المسارات</li>
+            <li>أعمالنا</li>
+            <li>فريقنا</li>
+        </ul> 
+        <button className='btn'>انضم لنا</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
